c5428d0651fc8830065e9a6bec2fd42b550252df
tienpbss <tienphung.cn07@gmail.com>
2023-08-10 13:14:41 +0700

    allow null description role

--- src/models/role.models.js
+++ src/models/role.models.js
@@ -10,7 +10,7 @@ module.exports = (sequelize) => {
         },
         description: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: true
         }
     })
     return Role
